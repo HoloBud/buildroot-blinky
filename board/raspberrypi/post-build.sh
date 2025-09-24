d5572d70a3c0b9da3c79fda3ac6f507e719b4cc8
luisgiii <loiis.x14@gmail.com>
2025-09-24 19:08:27 +0000

    fix: symbolic link creation to autostart services

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -13,14 +13,15 @@ elif [ -d ${TARGET_DIR}/etc/systemd ]; then
     mkdir -p "${TARGET_DIR}/etc/systemd/system/getty.target.wants"
     ln -sf /lib/systemd/system/getty@.service \
        "${TARGET_DIR}/etc/systemd/system/getty.target.wants/getty@tty1.service"
-    # Create the necessary symbolic links of the custom services
-    mkdir -p "${TARGET_DIR}/etc/systemd/system/multi-user.target.wants"
-    ln -sf /etc/systemd/system/wifi-dongle.service \
-       "${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/wifi-dongle.service"
-    
-    mkdir -p "${TARGET_DIR}/etc/systemd/system/wifi-dongle.service.wants"
-    ln -sf /etc/systemd/system/holo-device.service \
-       "${TARGET_DIR}/etc/systemd/system/wifi-dongle.service.wants/holo-device.service"
 fi
+
+# Create the necessary symbolic links of the custom services to be auto-called
+mkdir -p "${TARGET_DIR}/etc/systemd/system/multi-user.target.wants"
+ln -sf /etc/systemd/system/wifi-dongle.service \
+   "${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/wifi-dongle.service"
+
+mkdir -p "${TARGET_DIR}/etc/systemd/system/wifi-dongle.service.wants"
+ln -sf /etc/systemd/system/holo-device.service \
+   "${TARGET_DIR}/etc/systemd/system/wifi-dongle.service.wants/holo-device.service"
 
 # Delete unnecessary files after testing images if modifying configs turns out too challenging
